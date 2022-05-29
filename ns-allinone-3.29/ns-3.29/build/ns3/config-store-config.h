5cf002ab84fe586fbeaf6ad9abc125c73ddf791b
ilovethedogs <ittct2018@gmail.com>
2022-05-29 19:34:16 +0900

    changed the decision making algorithm

--- ns-allinone-3.29/ns-3.29/build/ns3/config-store-config.h
+++ ns-allinone-3.29/ns-3.29/build/ns3/config-store-config.h
@@ -3,8 +3,9 @@
 #ifndef W_NS3_CONFIG_STORE_CONFIG_H_WAF
 #define W_NS3_CONFIG_STORE_CONFIG_H_WAF
 
-#define PYTHONDIR "/usr/local/lib/python2.7/dist-packages"
-#define PYTHONARCHDIR "/usr/local/lib/python2.7/dist-packages"
-/* #undef HAVE_PYTHON_H */
+#define PYTHONDIR "/usr/local/lib/python3/dist-packages"
+#define PYTHONARCHDIR "/usr/local/lib/python3/dist-packages"
+/* #undef HAVE_PYEMBED */
+#define HAVE_GTK 1
 
 #endif /* W_NS3_CONFIG_STORE_CONFIG_H_WAF */
