58e59159fda463f4eab6c6d90b51b27d7ae2e930
ilovethedogs <ittct2018@gmail.com>
2022-05-29 19:48:27 +0900

    changed moving algorithm

--- ns-allinone-3.29/ns-3.29/src/applications/model/game-user.cc
+++ ns-allinone-3.29/ns-3.29/src/applications/model/game-user.cc
@@ -13,6 +13,9 @@
 #include "ns3/udp-socket-factory.h"
 #include "ns3/game-user.h"
 #include "ns3/string.h"
+#include "ns3/vector.h"
+#include <cmath>
+#include <utility>
 
 /*
 int moving_algorithm(std::string input_frame, int current_position)  
@@ -216,32 +219,114 @@ int moving_algorithm(std::string input_frame, int current_position)
 }
 */
 
-uint8_t moving_algorithm(std::string input_frame, int current_position) {
+uint8_t sub_abs_uint8(uint8_t a, uint8_t b) {
+    return a > b ? a - b : b - a;
+}
+
+std::vector<uint8_t> sub_each(std::vector<uint8_t>& v, uint8_t num) {
+    std::vector<uint8_t> result {};
+    for (auto& n : v) 
+        result.push_back(sub_abs_uint8(n, num));
+    return std::move(result);
+}
+
+std::vector<uint8_t> sub_each(std::vector<std::pair<uint8_t, uint8_t>>& v, uint8_t num) {
+    std::vector<uint8_t> v1 {};
+    for (auto& n : v)
+        v1.push_back(sub_abs_uint8(n.first, num));
+    return std::move(v1);
+}
+
+std::Compare
+
+uint8_t moving_algorithm(std::string input_frame, int current_pos) {
 	// move right = 2
 	// move left = 1
 	// stay = 0
-	uint8_t move = 0;
-    std::string upper0 {};
+	//uint8_t move = 0;
+    char upper0[11] = "";
     if (input_frame.copy(upper0, 10, 80) != 10) {
         std::cerr << "error at string copy.0" << std::endl;
         exit(1);
     }
-    std::string upper2 {};
+    else {
+        std::cout << "upper0: ";
+        std::cout << upper0 << std::endl;
+    }
+    char upper1[11] = "";
     if (input_frame.copy(upper1, 10, 70) != 10) {
         std::cerr << "error at string copy.1" << std::endl;
         exit(1);
     }
+    else {
+        std::cout << "upper1: ";
+        std::cout << upper1 << std::endl;
+    }
+    char upper2[11] = "";
+    if (input_frame.copy(upper2, 10, 60) != 10) {
+        std::cerr << "error at string copy.1" << std::endl;
+        exit(1);
+    }
+    else {
+        std::cout << "upper2: ";
+        std::cout << upper2 << std::endl;
+    }
 
-    /****
-     * 0110100101
-     * 0001010111
-     * ^8    ^
-     * the character examines two blocks lying right upper him
-     * and find the closest tile that are safe
-     */
+     /******
+     * 1111111011
+     * 0111010101
+     * 1000101011
+     *  aaa a a
+     * b   b b b
+     *        c
+     *     8
+     *
+     * the logic is fixed and very simple
+     * first character should find the "safe" tiles on each three blocks above it.
+     * then he calculate the "cost" on each safe tiles right upper him.
+     * "cost" can be calculated by the weight * distance from the character at that moment
+     * weight is 3 for upper0, 2 for upper1, 1 for upper2
+     * for example, we can calculated each "safe" tiles like this
+     * from the leftmost "a"
+     * a1 = (3 * 3) + (2 * 1) + (1 * 7) = 18
+     * a2 = min( (3 * 2) + (2 * 2) + (1 * 3) = 13, (3 * 2) + (2 * 2) + (1 * 7) = 17) = 13
+     * a3 = (3 * 1) + (2 * 1) + (1 * 3) = 8
+     * a4 = min( (3 * 1) + (2 * 1) + (1 * 3) = 8, (3 * 1) + (2 * 1) + (1 * 1) = 6 ) = 6
+     * a5 = (3 * 2) + (2 * 1) + (1 * 1) = 9
+     * the it will definitely choose the a4
+     * what if there are multiple minimum tiles?
+     * then choose the closest tile at upper0 + leftmost
+     *******/
+    
+    std::vector<uint8_t> upper2_safe {};
+    for (uint8_t i = 0; i != 10; ++i)
+        if (upper2[i] == '0') upper2_safe.push_back(i);
 
 
+    std::vector<std::pair<uint8_t, uint8_t>> upper0_safe {};
+    for (uint8_t i = 0; i != 10; ++i)
+        if (upper0[i] == '0') upper0_safe.emplace_back(std::make_pair(i, 3 * sub_abs_uint8(i, current_pos));
 
+    std::vector<std::pair<uint8_t, uint8_t>> upper1_safe {};
+    for (uint8_t i = 0; i != 10; ++i) {
+        if (upper1[i] == '0') {
+            std::vector<uint8_t> temp = sub_each(upper2_safe, i);
+            auto min = std::min_element(std::begin(temp), std::end(temp));
+            upper1_safe.emplace_back(i, *min);
+        }
+    }
+
+    std::vector<std::pair<uint8_t, uint8_t>> decision {};
+    for (auto& i : upper0_safe) {
+        std::vector<uint8_t> temp = sub_each(upper1_safe, i.first);
+        auto min = std::min_element(std::begin(temp), std::end(temp));
+        decision.emplace_back(i.first,
+                              i.second + 2 * (*min) + upper1_safe[std::distance(temp.begin(), min)].second);
+    }
+
+    return std::min_element(decision.begin(), decision.end(),
+                                      [](const std::pair<uint8_t, uint8_t>& a, const std::pair<uint8_t, uint8_t>& b)
+                                      { return a.second < b.second; })->first;
 }
 
 namespace ns3
