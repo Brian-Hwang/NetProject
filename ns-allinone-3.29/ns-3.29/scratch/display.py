a94376768837958997ca4b9229fe2ac90b2054d5
Brian Hwang <brian11hwangb@gmail.com>
2022-06-01 01:30:32 +0900

    Wifi Toplogy added

--- ns-allinone-3.29/ns-3.29/scratch/display.py
+++ ns-allinone-3.29/ns-3.29/scratch/display.py
@@ -7,31 +7,35 @@ RED = '\u001b[31m'
 GREEN = '\u001b[32m'
 
 square = bytes([254]).decode('cp437')
-top = bytes([201, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 187]).decode('cp437')
-bottom = bytes([200, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 188]).decode('cp437')
+top = bytes([201, 205, 205, 205, 205, 205, 205, 205,
+            205, 205, 205, 187]).decode('cp437')
+bottom = bytes([200, 205, 205, 205, 205, 205, 205, 205,
+               205, 205, 205, 188]).decode('cp437')
 n = 0
 size = 10
+
 
 def readNextFrame(size, f):
     frame = list(range(size))
     for i in range(size):
         frame[i] = list(range(size))
         for j in range(size):
-            frame[i][j] =  int(f.read(1))
+            frame[i][j] = int(f.read(1))
 
-    #if nextMove != "0":
+    # if nextMove != "0":
         #pos = frame[size-1].index(2)
         #frame[size-1][pos] = 0
 
-        #if nextMove == "1":
+        # if nextMove == "1":
             #pos = min(size-1, pos+1)
-        #if nextMove == "2":
+        # if nextMove == "2":
             #pos = max(0, pos-1)
-    
+
     #frame[size-1][pos] = 2
     return frame
 
-f = open("output.txt", "r")
+
+f = open("output1.txt", "r")
 #f2 = open("move.txt", "r")
 
 #firstFrame = [[0] * size] * size
@@ -61,7 +65,7 @@ while(n != 10000):
                     print(WHITE + "1", end="")
                 else:
                     #randVal = random.random()
-                    #if randVal > 0.9:
+                    # if randVal > 0.9:
                     if nextFrame[i-1][j-1] == 2:
                         #print(GREEN + square, end="")
                         print(GREEN + "8", end="")
@@ -69,15 +73,14 @@ while(n != 10000):
                     elif nextFrame[i-1][j-1] == 1:
                         #print(RED + square, end="")
                         print(RED + "0", end="")
-                    
+
                     else:
                         print(" ", end="")
                         #arr[j] = 0
         print("")
-            
+
     print(n)
     sleep(0.1)
-    n = n+ 1
+    n = n + 1
 
 f.close()
-
